be6b039c7359ca697c6a659b1c8c94cc31d93f05
ShadowOfTheRedFox <74469156+ShadowoftheRedFox@users.noreply.github.com>
2025-03-03 08:51:23 +0100

    base

---
+++ SuivieRapport.md
@@ -0,0 +1,11 @@
+# Introduction et contexte
+
+# Répartition des tâches
+
+# Étape réalisées
+
+- planification du projet:
+    - mise en place du dépôt Github
+    - organisation entre les membres du groupes (quels frameworks utilisés)
+
+# Conclusions et perspectives
--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { environment } from '../environments/environment';
+import { MainComponent } from './main/main.component';
+import { ErrorComponent } from './error/error.component';
 
-export const routes: Routes = [];
+const TITLE_POSTFIX = " - " + environment.title;
+
+export const routes: Routes = [
+    {
+        path: "",
+        title: "Accueil" + TITLE_POSTFIX,
+        component: MainComponent
+    }, {
+        path: "**",
+        title: "Erreur" + TITLE_POSTFIX,
+        component: ErrorComponent
+    }
+];
