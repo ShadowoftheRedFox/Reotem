b70d8c3e87696abd0866a33e7de04f40bcf037d9
ShadowoftheRedFox <leon.merin@gmail.com>
2025-03-24 22:00:08 +0100

    theme clean

--- front/src/services/theme.service.ts
+++ front/src/services/theme.service.ts
@@ -103,33 +103,6 @@ export class ThemeService {
             document.head.removeChild(existingLinkElement);
         }
     }
-
-    private changeTheme() {
-        this.auth.setCookie(ThemeCookie, this.themeChoosen.toString(), CookieTime.Year, "/");
-        // c'est dans cet élement qu'on stock no variable
-        const root = document.querySelector(':root');
-        if (!root) {
-            console.warn("no root element found");
-            return;
-        }
-        // prend les couleurs de la palette correspondante dans l'élement root
-        const pal_1 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-1`);
-        const pal_2 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-2`);
-        const pal_3 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-3`);
-        const pal_4 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-4`);
-        const pal_5 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-5`);
-        const pal_6 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-6`);
-        const pal_7 = getComputedStyle(root).getPropertyValue(`--pal${this.themeChoosen + 1}-7`);
-
-        // change les couleurs selon le theme
-        document.documentElement.style.setProperty('--color-1', pal_1);
-        document.documentElement.style.setProperty('--color-2', pal_2);
-        document.documentElement.style.setProperty('--color-3', pal_3);
-        document.documentElement.style.setProperty('--color-4', pal_4);
-        document.documentElement.style.setProperty('--color-5', pal_5);
-        document.documentElement.style.setProperty('--color-6', pal_6);
-        document.documentElement.style.setProperty('--color-7', pal_7);
-    }
 }
 
 
