4c4f3919e7616394de8a8423af1deb902b6394c3
ShadowoftheRedFox <leon.merin@gmail.com>
2025-04-01 00:27:59 +0200

    fix create form

--- front/src/app/test/test.component.ts
+++ front/src/app/test/test.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatTimepickerModule } from '@angular/material/timepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-test',
     imports: [
         RouterLink,
-        MatButtonModule
+        MatButtonModule,
+        MatTimepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
+    providers: [provideNativeDateAdapter()],
     templateUrl: './test.component.html',
     styleUrl: './test.component.scss'
 })
